3cdc4a0cfc3aa16867907eb985d888a9d5a9355a
Aishal Gupta <aishalgupta3008@gmail.com>
2022-08-11 22:52:29 +0530

    Add files via upload

---
+++ rect.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+ int t;
+ cin>>t;
+
+ while(t--){
+    int n;
+    cin>>n;
+
+    int arr[4*n];
+    for (int i=0;i<4*n;i++){
+        cin>>arr[i];
+    }
+
+    
+ }
+
+    return 0;
+}
